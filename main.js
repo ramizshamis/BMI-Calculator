19558b2c9751102a1f32c99fe9917c4eed27f222
ramizshamis <160166765+ramizshamis@users.noreply.github.com>
2024-09-09 20:58:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var weightinkg = 60;
+var heightinmeters = 1.65;
+var bmi = weightinkg / (heightinmeters * heightinmeters);
+// console.log(`your BMI is &{bmi}`);
+console.log(bmi);
+//output = 22.0385
